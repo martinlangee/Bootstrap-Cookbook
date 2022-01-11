00f48228f7379f3283ea4bba8d9cd6e866313410
martin <martin.lange@csa-germany.de>
2022-01-12 00:12:16 +0100

    introduce recipe-data.js

---
+++ recipe-data.js
@@ -0,0 +1,96 @@
+const recipeData = {
+    sections: [{
+            name: 'Salad',
+            recipes: []
+        },
+        {
+            name: 'Spice &amp; More',
+            recipes: [{
+                    title: 'Hunan Sauce',
+                    author: 'Julie Awad',
+                    date: '2020-11-16',
+                    time: '15 min',
+                    short: 'A blend of sweet and sour with hot and spicy',
+                    image: 'Dish-1.png',
+                    ingredients: [{
+                            ingredient: 'Cream',
+                            amount: '200 ml'
+                        },
+                        {
+                            ingredient: 'Peach pulp',
+                            amount: '400 g'
+                        },
+                        {
+                            ingredient: 'Black chocolate',
+                            amount: '200 g'
+                        }
+                    ],
+                    steps: [
+                        'Qui irure id Lorem proident sunt enim veniam exercitation culpa aliquip incididunt do nostrud ullamco.',
+                        'Deserunt ea voluptate veniam nostrud do.',
+                        'Non anim sunt amet nisi exercitation occaecat officia.',
+                        'Cillum dolore ea commodo eiusmod laborum mollit commodo reprehenderit in.'
+                    ],
+                },
+                {
+                    title: '',
+                    author: '',
+                    date: '',
+                    time: '',
+                    short: '',
+                    image: '',
+                    ingredients: [{
+                            ingredient: '',
+                            amount: ''
+                        },
+                        {
+                            ingredient: '',
+                            amount: ''
+                        },
+                        {
+                            ingredient: '',
+                            amount: ''
+                        }
+                    ],
+                    steps: [
+                        ''
+                    ],
+                },
+            ]
+        },
+        {
+            name: 'Rice',
+            recipes: []
+        },
+        {
+            name: 'Main Dish',
+            recipes: [{
+                title: 'T-Bone Steak',
+                author: 'John Doe',
+                date: '1965-05-11',
+                time: '4 min',
+                short: 'How to fry the steak right',
+                image: '',
+                ingredients: [{
+                        ingredient: 'T-Bone steak',
+                        amount: '1 pc of 400 g'
+                    },
+                    {
+                        ingredient: 'Oil',
+                        amount: '3 Spoons'
+                    },
+                    {
+                        ingredient: 'Salt',
+                        amount: 'Pinch'
+                    }
+                ],
+                steps: [
+                    'Amet non incididunt eiusmod dolor ea commodo id ut duis esse voluptate ex.',
+                    'In veniam nisi exercitation aute incididunt excepteur tempor nisi ea est in eiusmod irure ea.',
+                    'Culpa consequat elit officia commodo sunt ex cupidatat cupidatat ex.',
+                    'Ea quis deserunt enim ut labore id occaecat labore.'
+                ],
+            }, ]
+        },
+    ]
+};
