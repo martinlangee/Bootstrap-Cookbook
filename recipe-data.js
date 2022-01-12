2b8fb0a065be8cfe411c9c674c57178a692b9179
martin <martin.lange@csa-germany.de>
2022-01-12 17:46:32 +0100

    recipes are read from JSON file and HTML is created dynamically

--- recipe-data.js
+++ recipe-data.js
@@ -6,12 +6,13 @@ const recipeData = {
         {
             name: 'Spice &amp; More',
             recipes: [{
+                    id: 'hunan-sauce',
                     title: 'Hunan Sauce',
                     author: 'Julie Awad',
                     date: '2020-11-16',
-                    time: '15 min',
+                    time: '5 min',
                     short: 'A blend of sweet and sour with hot and spicy',
-                    image: 'Dish-1.png',
+                    imagePath: 'Dish-1.png',
                     comment: 'Sauce for one stir-fry recipe',
                     ingredients: [{
                             ingredient: 'Cream',
@@ -34,66 +35,106 @@ const recipeData = {
                     ],
                 },
                 {
-                    title: '',
-                    short: '',
-                    author: '',
-                    date: '',
-                    time: '',
-                    image: '',
-                    comment: '',
+                    id: 'austrian-guglhupf',
+                    title: 'Austrian Gugelhupf Cake',
+                    short: 'A recipe bringing back a lot of memories to Vienna',
+                    author: 'John Connor',
+                    date: '1997-08-29',
+                    time: '1 hour',
+                    imagePath: 'Dish-2.png',
+                    comment: 'Something you cannot resist',
                     ingredients: [{
-                            ingredient: '',
-                            amount: ''
+                            ingredient: 'Unsalted Butter (room temperature)',
+                            amount: '200 g'
                         },
                         {
-                            ingredient: '',
-                            amount: ''
+                            ingredient: 'Large Eggs',
+                            amount: '5'
                         },
                         {
-                            ingredient: '',
-                            amount: ''
-                        }
+                            ingredient: 'All-Purpose Flour',
+                            amount: '400 g'
+                        },
+                        {
+                            ingredient: 'Lemon Zest',
+                            amount: '1 pkg'
+                        },
+                        {
+                            ingredient: 'Vanilla Extract',
+                            amount: '1 pkg'
+                        },
                     ],
                     steps: [
-                        ''
+                        'Cillum fugiat nulla nisi et sint id elit cillum tempor reprehenderit.',
+                        'Laboris tempor id commodo amet labore irure eu nostrud velit exercitation cillum ut.',
+                        'Labore deserunt dolore et exercitation aliquip velit cillum nulla anim nisi ad elit excepteur exercitation. Eu et proident et sint.',
+                        'Et ullamco veniam nostrud Lorem nisi enim cupidatat velit adipisicing tempor incididunt aute exercitation culpa.',
+                        'Elit qui proident sint eu aliquip enim in ipsum veniam consectetur id anim aliquip reprehenderit.',
+                        'Incididunt proident laborum et esse esse ipsum laboris veniam.'
                     ],
                 },
             ]
         },
         {
             name: 'Rice',
-            recipes: []
+            recipes: [] //{
+                //     id: 'xxx',
+                //     title: '',
+                //     short: '',
+                //     author: '',
+                //     date: '',
+                //     time: '',
+                //     imagePath: '',
+                //     comment: '',
+                //     ingredients: [{
+                //             ingredient: '',
+                //             amount: ''
+                //         },
+                //         {
+                //             ingredient: '',
+                //             amount: ''
+                //         },
+                //         {
+                //             ingredient: '',
+                //             amount: ''
+                //         }
+                //     ],
+                //     steps: [
+                //         ''
+                //     ],
+                // }, ]
         },
         {
             name: 'Main Dish',
-            recipes: [{
-                title: 'T-Bone Steak',
-                short: 'How to fry the steak right',
-                author: 'John Doe',
-                date: '1965-05-11',
-                time: '4 min',
-                comment: '',
-                image: '',
-                ingredients: [{
-                        ingredient: 'T-Bone steak',
-                        amount: '1 pc of 400 g'
-                    },
-                    {
-                        ingredient: 'Oil',
-                        amount: '3 Spoons'
-                    },
-                    {
-                        ingredient: 'Salt',
-                        amount: 'Pinch'
-                    }
-                ],
-                steps: [
-                    'Amet non incididunt eiusmod dolor ea commodo id ut duis esse voluptate ex.',
-                    'In veniam nisi exercitation aute incididunt excepteur tempor nisi ea est in eiusmod irure ea.',
-                    'Culpa consequat elit officia commodo sunt ex cupidatat cupidatat ex.',
-                    'Ea quis deserunt enim ut labore id occaecat labore.'
-                ],
-            }, ]
+            recipes: [] //{
+                //     id: 't-bone-steak',
+                //     title: 'T-Bone Steak',
+                //     short: 'How to fry the steak right',
+                //     author: 'John Doe',
+                //     date: '1965-05-11',
+                //     time: '4 min',
+                //     comment: '',
+                //     imagePath: '',
+                //     ingredients: [{
+                //             ingredient: 'T-Bone steak',
+                //             amount: '1 pc of 400 g'
+                //         },
+                //         {
+                //             ingredient: 'Oil',
+                //             amount: '3 Spoons'
+                //         },
+                //         {
+                //             ingredient: 'Salt',
+                //             amount: 'Pinch'
+                //         }
+                //     ],
+                //     steps: [
+                //         'Amet non incididunt eiusmod dolor ea commodo id ut duis esse voluptate ex.',
+                //         'In veniam nisi exercitation aute incididunt excepteur tempor nisi ea est in eiusmod irure ea.',
+                //         'Culpa consequat elit officia commodo sunt ex cupidatat cupidatat ex.',
+                //         'Ea quis deserunt enim ut labore id occaecat labore.'
+                //     ],
+                // }, ]
         },
     ]
 };
--- script.js
+++ script.js
@@ -3,13 +3,88 @@ function calcTotalRecipeCount() {
 }
 
 function updateRecipeCount() {
-    const recipeCountHtml = document.getElementById('recipe-count');
-    recipeCountHtml.innerHTML = `${calcTotalRecipeCount()} Recipes`;
+    const recipeCountElem = document.getElementById('recipe-count');
+    recipeCountElem.innerHTML = `${calcTotalRecipeCount()} Recipes`;
 }
 
-function updateRecipeData() {
-    window.alert(recipeData.sections[0].name)
+function updateRecipeSelectionMenu() {
+    let html = '';
+    const recipeMenuElem = document.getElementById('recipe-menu');
+    recipeData.sections.forEach(sec => {
+        // add sections and recipes as from data
+        html += `<li><a class="dropdown-item">${sec.name}</a></li>\n`;
+    });
+    // add static HTML for "Add Section" item
+    html +=
+        '<li class="border-gray-top"></li>\n' +
+        '<li><a class="dropdown-item txt-darkred" href="#">Add Section</a></li>\n' +
+        '<li class="border-gray-top"></li>';
+
+    recipeMenuElem.innerHTML = html;
 }
 
-//updateRecipeData();
+function updateSelectedRecipe() {
+    let html = '';
+    recipeData.sections.forEach(sec => {
+        // add sections and recipes as from data
+        sec.recipes.forEach(rec => {
+            html += `
+                    <div class="recipe" id="${rec.id}">
+                        <div class="title">
+                            <h2>${rec.title}</h2>
+                            <div class="line-horizontal"></div>
+                        </div>
+                        <div class="description">
+                            ${rec.short}
+                        </div>
+                        <div class="image-container">
+                            <img class="recipe-image" src="./images/${rec.imagePath}">
+                            <div class="hide image-hint">${rec.title}</div>
+                        </div>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-6">
+                                    <p class="author-link"><a href="#">${rec.author}</a></p>
+                                    <p>${rec.comment}</p>
+                                </div>
+                                <div class="col-3">
+                                    <p class="total-time-header txt-gray">Total</p>
+                                    <p class="txt-gray">${rec.time}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <h4>Ingredients</h4>
+                        <div class="container">`;
+            rec.ingredients.forEach(ing => {
+                html += `
+                            <div class="row">
+                                <div class="col-6 border-gray-top">
+                                    <p>${ing.ingredient}</p>
+                                </div> 
+                                <div class="col-3 border-gray-top">
+                                    <p>${ing.amount}</p>
+                                </div>
+                            </div>`;
+            });
+            html += `
+                        </div>
+                        <h4>Steps</h4>
+                        <div class="container">
+                            <div class="row recipe-row">`;
+            rec.steps.forEach(step => {
+                html += `
+                                <p>${step}</p>`
+            });
+            html += `
+                            </div>
+                        </div>
+                    </div>`;
+        });
+    });
+    const recipeListElem = document.getElementById('recipe-list');
+    recipeListElem.innerHTML = html;
+}
+
 updateRecipeCount();
+updateRecipeSelectionMenu();
+updateSelectedRecipe();
